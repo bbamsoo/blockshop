f0426e3392f499f00c8114b2fd34ac1ec5dea399
bbamsoo <hiaro66@naver.com>
2023-06-19 10:19:05 +0900

    first commit

---
+++ src/main/java/com/example/blockshop/BlockshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.blockshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/blockshop/BlockshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blockshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
